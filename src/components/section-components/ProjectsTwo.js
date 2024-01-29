4101d786195f55d705e5d15e85901ddd5dca7415
Michał Jankiewicz <informatyk@dmdbudownictwo.pl>
2024-01-29 12:55:41 +0100

    aktualizacja realizacje

--- src/components/section-components/ProjectsTwo.js
+++ src/components/section-components/ProjectsTwo.js
@@ -47,18 +47,7 @@ const ProjectsTwo = () => {
                                 </figure>
                             </div>
                         </div> */}
-                        <div className="gallery-block-two">
-                            <div className="inner-box">
-                                <figure className="image-box">
-                                    <img src="images/gallery/gl-SB-750x650-RezydencjaParkowa.png" alt="" />
-                                    <div className="figcaption">
-                                        <h4>MG Projekt</h4>
-                                        <p>Budujemy marzenia od 1998. Twój dom, twój styl!</p>
-                                        <Link to="/single-realizacja-RezydencjaParkowa"><i className="link-plus bg-primary" /></Link>
-                                    </div>
-                                </figure>
-                            </div>
-                        </div>
+                        
                         <div className="image-effect-one hover-shadow">
                             <img src="images/gallery/gl-SB-750x650-RezydencjaParkowa.png" alt="" />
                             <div className="figcaption">
