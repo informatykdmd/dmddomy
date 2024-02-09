c3bfd3bc97bd0ca7a9556f9d122fc7290e7b13e0
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-02-09 18:48:39 +0100

    users

--- src/components/blog-components/UserDetailsChanges.js
+++ src/components/blog-components/UserDetailsChanges.js
@@ -8,9 +8,9 @@ const UserDetailsChanges = () => {
   const [userAvatar, setUserAvatar] = useState('');
   const [error, setError] = useState(null);
   const [avatarPreview, setAvatarPreview] = useState(null);
-  
+  const [successFull, setSuccessFull] = useState(false);
   const ApiAdres = myDatabaseConfig.mySqlUrlorIp + ':' + myDatabaseConfig.apiPort;
-  let SuccessFull = false;
+
   const handleAvatarChange = (event) => {
     const nowyAvatar = event.target.value;
     setUserAvatar(nowyAvatar);
@@ -30,7 +30,7 @@ const UserDetailsChanges = () => {
       console.log('userAvatar', userAvatar, typeof userAvatar);
       const response = await axios.post(`https://${ApiAdres}/api/addSubscriberAvatar`, { userHash, userAvatar });
       console.log(response.data);
-      SuccessFull = true;
+      setSuccessFull(true);
     } catch (error) {
       setError(error.message);
     }
@@ -45,14 +45,16 @@ const UserDetailsChanges = () => {
             <div className="row">
               <div className="col-md-12">
                 <form>
-                  <label>Adres URL obrazka avatara:</label>
-                  <input type="text" value={userAvatar} onChange={handleAvatarChange} />
-
-                  {/* Opcjonalnie: Wyświetl podgląd avatara */}
-                  {avatarPreview && <img src={avatarPreview} alt="Podgląd avatara" style={{ maxWidth: '100%', maxHeight: '150px' }} />}
-
-                  <button type="button" onClick={handleAvatarSubmit}>Aktualizuj avatar</button>
-                  {SuccessFull && <span style={{color: 'green'}}>Avatar został pomyślnie zmieniony.</span>}
+                  <div>
+                    <label>Adres URL obrazka avatara:</label>
+                  </div>
+                  <div>
+                    <input type="text" value={userAvatar} onChange={handleAvatarChange} />
+                    {/* Opcjonalnie: Wyświetl podgląd avatara */}
+                    {avatarPreview && <img src={avatarPreview} alt="Podgląd avatara" style={{ maxWidth: '100%', maxHeight: '150px' }} />}
+                    <button type="button" onClick={handleAvatarSubmit}>Aktualizuj avatar</button>
+                    {successFull && <span style={{color: 'green'}}>Avatar został pomyślnie zmieniony.</span> }
+                  </div>
                 </form>
               </div>
             </div>
