f8f453078028233a275d881a003f3286f87349c7
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-02-12 10:38:54 +0100

    oferta

--- src/elements/SidebarLeftParterowe.js
+++ src/elements/SidebarLeftParterowe.js
@@ -15,46 +15,46 @@ const SidebarLeft = () => {
             <ul>
                 <li>
                 <div className="blog-rp-image">
-                    <Link to="/single-project-WillapaPterowa2">
-                    <img src="images/offers/bl-01-120x120-Kasjopea7.png" alt="thumb" />
+                    <Link to="/single-project-WillaPterowa2">
+                    <img src="images/offers/bl-01-120x120-WillaPterowa2.png" alt="thumb" />
                     </Link>
                 </div>
                 <div className="blog-rp-info">
-                    <p><Link to="/single-project-WillapaPterowa2">Willa Parterowa 2</Link></p>
+                    <p><Link to="/single-project-WillaPterowa2">Willa Parterowa 2</Link></p>
                     <p>Zapraszamy do fascynującej podróży w świat nowoczesnej architektury z projektem domu Willa Parterowa 2.</p>
                 </div>
                 </li>
                 <li>
                 <div className="blog-rp-image">
-                    <Link to="/single-project-DomzWidokiem3">
-                    <img src="images/offers/bl-01-120x120-DomzWidokiem3.png" alt="thumb" />
+                    <Link to="/single-project-DomWaski3">
+                    <img src="images/offers/bl-01-120x120-DomWaski3.png" alt="thumb" />
                     </Link>
                 </div>
                 <div className="blog-rp-info">
-                    <p><Link to="/single-project-DomzWidokiem3">Wąski 3</Link></p>
+                    <p><Link to="/single-project-DomWaski3">Wąski 3</Link></p>
                     <p>Projekt domu Wąski 3 to nowa odsłona popularnej serii domów, idealnie dostosowana do wąskich działek.</p>
                 </div>
                 </li>
                 <li>
                 <div className="blog-rp-image">
-                    <Link to="/single-project-OpalElegancja">
-                    <img src="images/offers/bl-01-120x120-OpalElegancja.png" alt="thumb" />
+                    <Link to="/single-project-WillaParkowa4">
+                    <img src="images/offers/bl-01-120x120-WillaParkowa4.png" alt="thumb" />
                     </Link>
                 </div>
                 <div className="blog-rp-info">
-                    <p><Link to="/single-project-OpalElegancja">Willa Parkowa 4</Link></p>
-                    <p>Willa Parkowa 4 to jednokondygnacyjny dom o prostokątnej bryle, z czterospadowym dachem i przestrzenią garażową.</p>
+                    <p><Link to="/single-project-WillaParkowa4">Willa Parkowa 4</Link></p>
+                    <p>Willa Parkowa 4 to jednokondygnacyjny dom o prostokątnej bryle, z czterospadowym dachem i garażem.</p>
                 </div>
                 </li>
                 <li>
                 <div className="blog-rp-image">
-                    <Link to="/single-project-willaParkowa160">
-                    <img src="images/offers/bl-01-120x120-willaParkowa160.png" alt="thumb" />
+                    <Link to="/single-project-DomNaDebowej">
+                    <img src="images/offers/bl-01-120x120-DomNaDebowej.png" alt="thumb" />
                     </Link>
                 </div>
                 <div className="blog-rp-info">
-                    <p><Link to="/single-project-willaParkowa160">Dom Na Dębowej</Link></p>
-                    <p>Dom na Dębowej to projekt domu przeznaczonego dla czteroosobowej rodziny, o charakterystycznej stylistyce dworkowej.</p>
+                    <p><Link to="/single-project-DomNaDebowej">Dom Na Dębowej</Link></p>
+                    <p>Dom na Dębowej to projekt domu przeznaczonego dla czteroosobowej rodziny, w stylistyce dworkowej.</p>
                 </div>
                 </li>
             </ul>
