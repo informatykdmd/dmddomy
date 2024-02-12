c8e22722111968cc140f79277a4e89dfbbce084e
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-02-12 10:34:41 +0100

    oferta

--- src/elements/SidebarLeftParterowe.js
+++ src/elements/SidebarLeftParterowe.js
@@ -15,13 +15,13 @@ const SidebarLeft = () => {
             <ul>
                 <li>
                 <div className="blog-rp-image">
-                    <Link to="/single-project-Kasjopea7">
+                    <Link to="/single-project-WillapaPterowa2">
                     <img src="images/offers/bl-01-120x120-Kasjopea7.png" alt="thumb" />
                     </Link>
                 </div>
                 <div className="blog-rp-info">
-                    <p><Link to="/single-project-Kasjopea7">Kasjopea 7</Link></p>
-                    <p>Zapraszamy do fascynującej podróży w świat nowoczesnej architektury z projektem domu Kasjopea 7.</p>
+                    <p><Link to="/single-project-WillapaPterowa2">Willa Parterowa 2</Link></p>
+                    <p>Zapraszamy do fascynującej podróży w świat nowoczesnej architektury z projektem domu Willa Parterowa 2.</p>
                 </div>
                 </li>
                 <li>
@@ -31,8 +31,8 @@ const SidebarLeft = () => {
                     </Link>
                 </div>
                 <div className="blog-rp-info">
-                    <p><Link to="/single-project-DomzWidokiem3">Dom z Widokiem 3</Link></p>
-                    <p>Odkryj projekt "Domu z Widokiem 3" – 187 m2 doskonałego połączenia komfortu, funkcjonalności i imponującego widoku.</p>
+                    <p><Link to="/single-project-DomzWidokiem3">Wąski 3</Link></p>
+                    <p>Projekt domu Wąski 3 to nowa odsłona popularnej serii domów, idealnie dostosowana do wąskich działek.</p>
                 </div>
                 </li>
                 <li>
@@ -42,8 +42,8 @@ const SidebarLeft = () => {
                     </Link>
                 </div>
                 <div className="blog-rp-info">
-                    <p><Link to="/single-project-OpalElegancja">Opal</Link></p>
-                    <p>Rozpocznij podróż z projektem Opal o imponującej powierzchni 189 m2.</p>
+                    <p><Link to="/single-project-OpalElegancja">Willa Parkowa 4</Link></p>
+                    <p>Willa Parkowa 4 to jednokondygnacyjny dom o prostokątnej bryle, z czterospadowym dachem i przestrzenią garażową.</p>
                 </div>
                 </li>
                 <li>
@@ -53,98 +53,10 @@ const SidebarLeft = () => {
                     </Link>
                 </div>
                 <div className="blog-rp-info">
-                    <p><Link to="/single-project-willaParkowa160">Willa Parkowa</Link></p>
-                    <p>Zapraszamy w świat budowy, gdzie projekt "Willa Parkowa" staje się symbolem elegancji i wyrafinowanego stylu.</p>
+                    <p><Link to="/single-project-willaParkowa160">Dom Na Dębowej</Link></p>
+                    <p>Dom na Dębowej to projekt domu przeznaczonego dla czteroosobowej rodziny, o charakterystycznej stylistyce dworkowej.</p>
                 </div>
                 </li>
-                {/* <li>
-                <div className="blog-rp-image">
-                    <Link to="/single-project-Tytan134">
-                    <img src="images/offers/bl-01-120x120-Tytan134.png" alt="thumb" />
-                    </Link>
-                </div>
-                <div className="blog-rp-info">
-                    <p><Link to="/single-project-Tytan134">Tytan</Link></p>
-                    <p>Rozpocznij budowę swojego wymarzonego domu z projektem "Tytna".</p>
-                </div>
-                </li>
-                <li>
-                <div className="blog-rp-image">
-                    <Link to="/single-project-Turkus150">
-                    <img src="images/offers/bl-01-120x120-Turkus150.png" alt="thumb" />
-                    </Link>
-                </div>
-                <div className="blog-rp-info">
-                    <p><Link to="/single-project-Turkus150">Turkus</Link></p>
-                    <p>Nowoczesne podejście do architektury na 150 m2, gdzie funkcjonalność spotyka się z estetyką.</p>
-                </div>
-                </li>
-                <li>
-                <div className="blog-rp-image">
-                    <Link to="/single-project-Riwiera5">
-                    <img src="images/offers/bl-01-120x120-Riwiera5.png" alt="thumb" />
-                    </Link>
-                </div>
-                <div className="blog-rp-info">
-                    <p><Link to="/single-project-Riwiera5">Riwiera 5</Link></p>
-                    <p>Wkraczając w świat projektu "Riwiera 5", odkryj harmonię elegancji i przestrzeni na 197 metrach kwadratowych.</p>
-                </div>
-                </li>
-                <li>
-                <div className="blog-rp-image">
-                    <Link to="/single-project-Szafir181">
-                    <img src="images/offers/bl-01-120x120-Szafir181.png" alt="thumb" />
-                    </Link>
-                </div>
-                <div className="blog-rp-info">
-                    <p><Link to="/single-project-Szafir181">Szafir</Link></p>
-                    <p>Ekskluzywny design spotyka się z funkcjonalnością na 181 m2, tworząc przestrzeń unikalnego stylu życia.</p>
-                </div>
-                </li>
-                <li>
-                <div className="blog-rp-image">
-                    <Link to="/single-project-Szmaragd134">
-                    <img src="images/offers/bl-01-120x120-Szmaragd134.png" alt="thumb" />
-                    </Link>
-                </div>
-                <div className="blog-rp-info">
-                    <p><Link to="/single-project-Szmaragd134">Szmaragd</Link></p>
-                    <p>Projekt "Szmaragd 134", mimo skromnych wymiarów, ta budowa emanuje wielką elegancją, oferując inteligentne rozwiązania.</p>
-                </div>
-                </li>
-                <li>
-                <div className="blog-rp-image">
-                    <Link to="/single-project-Modena147">
-                    <img src="images/offers/bl-01-120x120-Modena147.png" alt="thumb" />
-                    </Link>
-                </div>
-                <div className="blog-rp-info">
-                    <p><Link to="/single-project-Modena147">Modena</Link></p>
-                    <p>Wprowadź nowoczesność do swojego życia z projektem "Modena", odkryjesz nie tylko funkcjonalność, ale także wyjątkowy styl.</p>
-                </div>
-                </li>
-                <li>
-                <div className="blog-rp-image">
-                    <Link to="/single-project-Kasjopea154">
-                    <img src="images/offers/bl-01-120x120-Kasjopea154.png" alt="thumb" />
-                    </Link>
-                </div>
-                <div className="blog-rp-info">
-                    <p><Link to="/single-project-Kasjopea154">Kasjopea</Link></p>
-                    <p>Na 154 m2 spotkasz harmonię elegancji i funkcjonalności, tworząc przestrzeń, która spełnia najwyższe standardy.</p>
-                </div>
-                </li>
-                <li>
-                <div className="blog-rp-image">
-                    <Link to="/single-project-Helios157">
-                    <img src="images/offers/bl-01-120x120-Helios157.png" alt="thumb" />
-                    </Link>
-                </div>
-                <div className="blog-rp-info">
-                    <p><Link to="/single-project-Helios157">Helios</Link></p>
-                    <p>Wprowadź promienie słońca do swojego nowego domu z projektem "Helios", tworząc przestrzeń, która emanuje ciepłem i nowatorskim stylem.</p>
-                </div>
-                </li> */}
             </ul>
             </div>
         </div>
