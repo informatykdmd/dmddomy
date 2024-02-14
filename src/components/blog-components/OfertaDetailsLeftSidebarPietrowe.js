661171d24b65f77fe236e57da0810d77d3e0e5d6
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-02-14 14:29:13 +0100

    oferta

--- src/components/blog-components/OfertaDetailsLeftSidebarPietrowe.js
+++ src/components/blog-components/OfertaDetailsLeftSidebarPietrowe.js
@@ -39,74 +39,75 @@ const BlogDetailsLeftSidebar = () => {
             <div className="col-md-8 order-md-2">
               <div className="blog-detail mt-30">
                 
-                <div className="det-content">
-                    <h3>Rozpocznij z Nami Nową Przygodę <br />Najnowsze Koncepcje Domy z Poddaszem</h3>
-                    <blockquote>
-                        <p>
-                            Mamy przyjemność zaprezentować nasze innowacyjne podejście do budowy domów z poddaszem, stworzonych z myślą o różnorodnych oczekiwaniach naszych Klientów. 
-                            Nasza firma zdobyła uznanie dzięki realizacji unikalnych, nowoczesnych projektów, oferujących domy z poddaszem w zaskakujących formach i funkcjonalnościach.
-                        </p>
-                    </blockquote>
-                    <p>
-                        Idąc na przekór tradycji, proponujemy konkurencyjne ceny oparte na naszym głębokim doświadczeniu w tworzeniu innowacyjnych domów z poddaszem w Warszawie 
-                        i okolicy. Specjalizujemy się w budowie domów z poddaszem według nowatorskich projektów, takich jak Hiacynt 3, Narcyz 2, Zefir czy Dom Na Parkowej 6.
-                    </p>
-                    <p>
-                        Nasza oferta obejmuje domy z poddaszem o różnych metrażach, od kompaktowych po przestronne rezydencje, 
-                        z garażami jedno- lub dwustanowiskowymi, a także alternatywnymi rozwiązaniami, takimi 
-                        jak dodatkowa przestrzeń do pracy. Dzięki naszym osiągnięciom w projektach, 
-                        zapewniamy, że koszt budowy domu z poddaszem z naszym doświadczonym zespołem będzie w doskonałej 
-                        relacji do oferowanej jakości.
-                    </p>                  
-                </div>
-
+              <div className="det-content">
+                <h3>Rozpocznij z Nami Nową Przygodę <br />Najnowsze Koncepcje Domy Piętrowe</h3>
                 <blockquote>
                     <p>
-                        “Zapraszamy Cię do ekscytującej podróży przez świat nowoczesnych projektów domów z poddaszem, 
-                        gdzie nowatorska jakość łączy się z głębokim doświadczeniem, tworząc wyjątkowe przestrzenie gotowe 
-                        na spełnienie marzeń każdego Klienta. Nasza firma to nie tylko budowa domów z poddaszem, 
-                        lecz kreowanie unikalnych historii w betonie i cegle. Każdy projekt, od minimalistycznych domów 
-                        z poddaszem po ekskluzywne rezydencje z poddaszem, jest rezultatem pasji, zaangażowania i wyjątkowego kunsztu. 
-                        Pragniemy, aby Twój dom z poddaszem był nie tylko miejscem zamieszkania, lecz autentycznym odzwierciedleniem 
-                        Twoich marzeń i aspiracji. Odkryj z nami przestrzeń, gdzie każdy detal ma znaczenie, a jakość staje się synonimem 
-                        naszej misji.”
+                        Mamy przyjemność zaprezentować nasze innowacyjne podejście do budowy domów piętrowych, stworzonych z myślą o różnorodnych oczekiwaniach naszych Klientów. 
+                        Nasza firma zdobyła uznanie dzięki realizacji unikalnych, nowoczesnych projektów, oferujących domy piętrowe w zaskakujących formach i funkcjonalnościach.
                     </p>
                 </blockquote>
+                <p>
+                    Idąc na przekór tradycji, proponujemy konkurencyjne ceny oparte na naszym głębokim doświadczeniu w tworzeniu innowacyjnych domów piętrowych w Warszawie 
+                    i okolicy. Specjalizujemy się w budowie domów piętrowych według nowatorskich projektów, takich jak Amarant 4, Iris 3, Aquila czy Dom Na Różanej 7.
+                </p>
+                <p>
+                    Nasza oferta obejmuje domy piętrowe o różnych metrażach, od kompaktowych po przestronne rezydencje, 
+                    z garażami jedno- lub dwustanowiskowymi, a także alternatywnymi rozwiązaniami, takimi 
+                    jak dodatkowe pomieszczenia do pracy czy relaksu. Dzięki naszym osiągnięciom w projektach, 
+                    zapewniamy, że koszt budowy domu piętrowego z naszym doświadczonym zespołem będzie w doskonałej 
+                    relacji do oferowanej jakości.
+                </p>                  
+            </div>
 
-                <div className="det-list">
-                  <div className="row">
-                    <div className="col-sm-7">
-                    <ul>
-                        <li>
-                            <p> Innowacje w Konstrukcji Domów z Poddaszem: Poznaj z nami nowoczesne podejście do tworzenia wyjątkowych przestrzeni z poddaszem, 
-                            gdzie jakość stoi na pierwszym miejscu. Każdy projekt to nie tylko budowa domu z poddaszem, lecz także spełnienie marzeń 
-                            i aspiracji.</p>
-                        </li>
-                        <li>
-                            <p> Doświadczenie Mówiące samo za Siebie: Nasze obszerne doświadczenie widoczne jest w każdym detalu. Połączenie jakości 
-                            z głębokim fachowym know-how tworzy niepowtarzalne przestrzenie z poddaszem, które są żywą opowieścią o naszych 
-                            umiejętnościach.</p>
-                        </li>
-                        <li>
-                            <p> Projekty Dla Ciebie: Oferujemy różnorodność nowoczesnych projektów z poddaszem, dostosowanych do indywidualnych 
-                            potrzeb. Niezależnie od tego, czy marzysz o kompaktowym domu z poddaszem czy ekskluzywnej rezydencji z poddaszem, 
-                            każdy projekt jest stworzony z myślą o spełnieniu Twoich marzeń.</p>
-                        </li>
-                        <li>
-                            <p> Dom jako Opowieść: Nasza firma nie tylko buduje domy z poddaszem, lecz tworzy historie w betonie i cegle. Chcemy, 
-                            aby Twój dom z poddaszem był wyjątkowym odzwierciedleniem Twojej osobowości. Odkryj z nami przestrzeń, gdzie dbałość 
-                            o detale i jakość stają się integralną częścią naszej misji.</p>
-                        </li>
-                    </ul>
-                    </div>
+            <blockquote>
+                <p>
+                    “Zapraszamy Cię do ekscytującej podróży przez świat nowoczesnych projektów domów piętrowych, 
+                    gdzie nowatorska jakość łączy się z głębokim doświadczeniem, tworząc wyjątkowe przestrzenie gotowe 
+                    na spełnienie marzeń każdego Klienta. Nasza firma to nie tylko budowa domów piętrowych, 
+                    lecz kreowanie unikalnych historii w betonie i cegle. Każdy projekt, od minimalistycznych domów 
+                    piętrowych po ekskluzywne rezydencje piętrowe, jest rezultatem pasji, zaangażowania i wyjątkowego kunsztu. 
+                    Pragniemy, aby Twój dom piętrowy był nie tylko miejscem zamieszkania, lecz autentycznym odzwierciedleniem 
+                    Twoich marzeń i aspiracji. Odkryj z nami przestrzeń, gdzie każdy detal ma znaczenie, a jakość staje się synonimem 
+                    naszej misji.”
+                </p>
+            </blockquote>
 
-                    <div className="col-sm-5">
-                      <p><img src="images/offers/bl-09-750x430-Poddasze1.png" alt="blog" /></p>
-                      <p><img src="images/offers/bl-09-750x430-Poddasze2.png" alt="blog" /></p>
-                      <p><img src="images/offers/bl-09-750x430-Poddasze3.png" alt="blog" /></p>
-                    </div>
-                  </div>
+            <div className="det-list">
+              <div className="row">
+                <div className="col-sm-7">
+                <ul>
+                    <li>
+                        <p> Innowacje w Konstrukcji Domów Piętrowych: Poznaj z nami nowoczesne podejście do tworzenia wyjątkowych przestrzeni piętrowych, 
+                        gdzie jakość stoi na pierwszym miejscu. Każdy projekt to nie tylko budowa domu piętrowego, lecz także spełnienie marzeń 
+                        i aspiracji.</p>
+                    </li>
+                    <li>
+                        <p> Doświadczenie Mówiące samo za Siebie: Nasze obszerne doświadczenie widoczne jest w każdym detalu. Połączenie jakości 
+                        z głębokim fachowym know-how tworzy niepowtarzalne przestrzenie piętrowe, które są żywą opowieścią o naszych 
+                        umiejętnościach.</p>
+                    </li>
+                    <li>
+                        <p> Projekty Dla Ciebie: Oferujemy różnorodność nowoczesnych projektów piętrowych, dostosowanych do indywidualnych 
+                        potrzeb. Niezależnie od tego, czy marzysz o kompaktowym domu piętrowym czy ekskluzywnej rezydencji piętrowej, 
+                        każdy projekt jest stworzony z myślą o spełnieniu Twoich marzeń.</p>
+                    </li>
+                    <li>
+                        <p> Dom jako Opowieść: Nasza firma nie tylko buduje domy piętrowe, lecz tworzy historie w betonie i cegle. Chcemy, 
+                        aby Twój dom piętrowy był wyjątkowym odzwierciedleniem Twojej osobowości. Odkryj z nami przestrzeń, gdzie dbałość 
+                        o detale i jakość stają się integralną częścią naszej misji.</p>
+                    </li>
+                </ul>
                 </div>
+
+                <div className="col-sm-5">
+                  <p><img src="images/offers/bl-09-750x430-Pietrowe1.png" alt="blog" /></p>
+                  <p><img src="images/offers/bl-09-750x430-Pietrowe2.png" alt="blog" /></p>
+                  <p><img src="images/offers/bl-09-750x430-Pietrowe3.png" alt="blog" /></p>
+                </div>
+              </div>
+            </div>
+
                 <div className="blog-tags">
                   <div className="row">
                     <div className="col-md-4 text-right">
@@ -131,7 +132,7 @@ const BlogDetailsLeftSidebar = () => {
                 <div className="col-md-8">
                   <span className="title">Sprawdź również : </span>
                   <Link to="/oferta-parterowe" className="tag">Domy parterowe</Link>
-                  <Link to="/oferta-pietrowe" className="tag">Domy piętrowe</Link>
+                  <Link to="/oferta-poddasze" className="tag">Domy z poddaszem</Link>
                   <Link to="/oferta-wille" className="tag">Wille</Link>
                   <Link to="/oferta-nowoczesne" className="tag">W nowoczesnej zabudowie</Link>
                 </div>
