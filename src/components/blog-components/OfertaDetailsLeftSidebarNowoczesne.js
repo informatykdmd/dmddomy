f40698d5d357e616615766173914956f17caea81
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-02-16 07:52:26 +0100

    oferta

--- src/components/blog-components/OfertaDetailsLeftSidebarNowoczesne.js
+++ src/components/blog-components/OfertaDetailsLeftSidebarNowoczesne.js
@@ -40,35 +40,35 @@ const BlogDetailsLeftSidebar = () => {
               <div className="blog-detail mt-30">
                 
             <div className="det-content">
-                <h3>Odkryj z Nami Nowoczesne Stodoły <br />Najnowsze Trendy w Naszej Ofertce - Eleganckie Wille Typu Stodoła</h3>
+                <h3>Odkryj z Nami Nowoczesne Domy <br />Najnowsze Trendy w Naszej Ofertce - Eleganckie Wille Typu Stodoła</h3>
                 <blockquote>
                     <p>
-                        Z entuzjazmem prezentujemy naszą ofertę budowy eleganckich willi w stylu stodoła z gotowych projektów, dostosowanych
+                        Z entuzjazmem prezentujemy naszą ofertę budowy eleganckich willi z gotowych projektów, dostosowanych
                         do różnorodnych oczekiwań naszych Klientów. Nasza firma zdobyła uznanie dzięki realizacji kilku imponujących,
-                        funkcjonalnych willi typu stodoła o zróżnicowanych rozmiarach. Oferujemy zarówno mniejsze domy, idealne dla tych,
-                        którzy pragną kameralnej przestrzeni, jak i reprezentacyjne wille w stylu stodoła, dostojne i przestronne.
+                        funkcjonalnych willi w nowoczenej zabudowie o zróżnicowanych rozmiarach. Oferujemy zarówno mniejsze domy, idealne dla tych,
+                        którzy pragną kameralnej przestrzeni, jak i reprezentacyjne wille, dostojne i przestronne.
                     </p>
                 </blockquote>
                 <p>
                     Wychodząc naprzeciw oczekiwaniom naszych Klientów, proponujemy konkurencyjne ceny oparte na naszym bogatym doświadczeniu
-                    wynikającym z licznych realizacji projektów willi w stylu stodoła na zachodzie Warszawy i okolicach. Specjalizujemy się
-                    w budowie nowoczesnych domów w stylu stodoła, takich jak Willa Stodołowa 2, Wąska 3, Willa Parkowa 4, Dom Na Dębowej.
+                    wynikającym z licznych realizacji projektów domów w stylu nowoczenym na zachodzie Warszawy i okolicach. Specjalizujemy się
+                    w budowie nowoczesnych domów w stylu stodoła, takich jak Sensowny 2, Ekonomiczny 2, Dom Na Praktycznej 2 czy Wymarzony 9.
                 </p>
                 <p>
-                    Nasza oferta obejmuje eleganckie wille w stylu stodoła o różnych metrażach, od mniejszych po przestronne rezydencje,
+                    Nasza oferta obejmuje eleganckie wille o różnych metrażach, od mniejszych po przestronne rezydencje,
                     z garażami na jedno lub dwa stanowiska, a także alternatywnymi rozwiązaniami, takimi jak dodatkowa przestrzeń sypialna.
-                    Dzięki liczbowym sukcesom w realizacjach, gwarantujemy, że koszt budowy willi typu stodoła z naszym doświadczonym zespołem
+                    Dzięki liczbowym sukcesom w realizacjach, gwarantujemy, że koszt budowy domu typu stodoła z naszym doświadczonym zespołem
                     będzie w optymalnej relacji do jakości.
                 </p>      
             </div>
 
             <blockquote>
                 <p>
-                    "Zapraszamy Cię do fascynującej podróży przez świat nowoczesnych projektów willi w stylu stodoła, gdzie doskonała jakość
+                    "Zapraszamy Cię do fascynującej podróży przez świat nowoczesnych projektów, gdzie doskonała jakość
                     spotyka się z głębokim doświadczeniem, formując niezrównane przestrzenie gotowe na spełnienie marzeń każdego Klienta.
-                    Nasza firma to nie tylko budowa eleganckich willi w stylu stodoła, lecz tworzenie unikalnych historii w betonie i drewnie.
-                    Każdy projekt, począwszy od skromniejszych domów w stylu stodoła po reprezentacyjne wille, jest wynikiem pasji, zaangażowania
-                    i wyjątkowego kunsztu. Pragniemy, aby Twój dom w stylu stodoła był nie tylko miejscem zamieszkania, lecz prawdziwym
+                    Nasza firma to nie tylko budowa eleganckich domów, lecz tworzenie unikalnych historii w betonie i drewnie.
+                    Każdy projekt, począwszy od skromniejszych domów po reprezentacyjne wille, jest wynikiem pasji, zaangażowania
+                    i wyjątkowego kunsztu. Pragniemy, aby Twój dom był nie tylko miejscem zamieszkania, lecz prawdziwym
                     odzwierciedleniem Twoich marzeń i aspiracji. Odkryj z nami przestrzeń, gdzie każdy detal ma znaczenie, a jakość staje się
                     synonimem naszej misji."
                 </p>
@@ -80,22 +80,22 @@ const BlogDetailsLeftSidebar = () => {
                     <ul>
                         <li>
                             <p>Pasja w Budowie Eleganckich Willi w Stylu Stodoła: Odkryj z nami pasję w tworzeniu niepowtarzalnych
-                            przestrzeni w stylu stodoła, gdzie jakość jest naszym priorytetem. Każdy projekt to nie tylko budowa
-                            domu w stylu stodoła, ale także realizacja marzeń i aspiracji.</p>
+                            przestrzeni, gdzie jakość jest naszym priorytetem. Każdy projekt to nie tylko budowa
+                            domu, ale także realizacja marzeń i aspiracji.</p>
                         </li>
                         <li>
                             <p>Doświadczenie, Które Mówi Głośno: Nasze bogate doświadczenie jest widoczne w każdym detalu.
-                            Zderzenie jakości z głębokim fachowym know-how tworzy niezrównane przestrzenie w stylu stodoła,
+                            Zderzenie jakości z głębokim fachowym know-how tworzy niezrównane przestrzenie,
                             które stają się żywą opowieścią o naszych umiejętnościach.</p>
                         </li>
                         <li>
-                            <p>Projekty Dla Ciebie: Oferujemy różnorodność gotowych projektów w stylu stodoła, dopasowanych do
-                            indywidualnych potrzeb. Niezależnie od tego, czy marzysz o skromniejszym domu w stylu stodoła czy
+                            <p>Projekty Dla Ciebie: Oferujemy różnorodność gotowych projektów, dopasowanych do
+                            indywidualnych potrzeb. Niezależnie od tego, czy marzysz o skromniejszym domu czy
                             reprezentacyjnej willi, każdy projekt jest stworzony z myślą o spełnieniu Twoich marzeń.</p>
                         </li>
                         <li>
-                            <p>Dom jako Historia: Nasza firma nie tylko buduje eleganckie wille w stylu stodoła, lecz tworzy
-                            historie w betonie i drewnie. Chcemy, aby Twój dom w stylu stodoła był wyjątkowym odzwierciedleniem
+                            <p>Dom jako Historia: Nasza firma nie tylko buduje eleganckie wille, lecz tworzy
+                            historie wielu zadowolonych klientów. Chcemy, aby Twój dom był wyjątkowym odzwierciedleniem
                             Twojej osobowości. Odkryj z nami przestrzeń, gdzie dbałość o detale i jakość stają się integralną
                             częścią naszej misji.</p>
                         </li>
