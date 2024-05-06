36ed4c8f13c8574f2a99b5a9fe82559514524a55
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-05-06 08:15:49 +0200

    Working now

--- src/pages/innerpages/Advice.js
+++ src/pages/innerpages/Advice.js
@@ -3,6 +3,7 @@ import HeaderOne from '../../common/header/HeaderOne';
 import BreadcrumbOneOnas from '../../common/breadcrumb/BreadcrumbOneOnas';
 import ProjectsDetailsOneKonsultacje from '../../components/project-components/ProjectsDetailsOneKonsultacje';
 import Quote from '../../components/section-components/Quote';
+import Counter from '../../components/section-components/Counter';
 import FooterOne from '../../common/footer/FooterOne';
 import $ from 'jquery';
 
@@ -23,7 +24,7 @@ const Advice = () => {
         />
         
         <ProjectsDetailsOneKonsultacje />
-        
+
         <Counter />
         
         <Quote />
